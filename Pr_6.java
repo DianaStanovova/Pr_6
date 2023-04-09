7c68601960e63517b4d463068f0ccb53b0a68b4e
DianaStanovova <126920304+DianaStanovova@users.noreply.github.com>
2023-04-09 16:48:04 +0300

    Add files via upload

---
+++ Pr_6.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pr_6;
+
+/**
+ *
+ * @author sogri
+ */
+public class Pr_6 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
